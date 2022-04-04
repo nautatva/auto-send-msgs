2efd72b87500b2f54bcbb53b7c33fdd12378618f
Nautatava Navlakha <nautatava@finmechanics.com>
2022-04-04 13:25:53 +0530

    move common code to common_utils

--- backend/utils/common_utils.py
+++ backend/utils/common_utils.py
@@ -1,3 +1,17 @@
+def merge_iterables(iter1, iter2):
+    # TODO: make a function with *args instead of fixed args
+    final_list = []
+    if iter1 is None:
+        return iter2
+    if iter2 is None:
+        return iter1
+    for e in iter1:
+        final_list.append(e)
+    for e in iter2:
+        final_list.append(e)
+
+    return final_list
+
 
 def is_json_key_present(json:dict, key:str) -> bool:
     # TODO: Add in common_utils PyPi
--- backend/utils/django_utils.py
+++ backend/utils/django_utils.py
@@ -2,42 +2,23 @@ from django.contrib import admin
 from django.conf import settings
 from django.db import models
 from django.db.models import Q
+from utils.common_utils import merge_iterables
 
 
 class BaseAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        # TODO: Filter queryset to allow only self-created objects
-        return super(BaseAdmin, self).get_queryset(request)
+    # def get_queryset(self, request):
+    #     # TODO: Filter queryset to allow only self-created objects
+    #     return super(BaseAdmin, self).get_queryset(request)
 
     def get_exclude(self, request, obj=None):
         custom_hidden_fields = ["created_by", "last_modified_by"]
-        hidden_fields = []
         concrete_class_hidden_fields = super(BaseAdmin, self).get_exclude(request, obj)
-        if concrete_class_hidden_fields is None:
-            return custom_hidden_fields
-
-        for field in concrete_class_hidden_fields:
-            hidden_fields.append(field)
-
-        for field in custom_hidden_fields:
-            hidden_fields.append(field)
-
-        return hidden_fields
+        return merge_iterables(concrete_class_hidden_fields, custom_hidden_fields)
 
     def get_readonly_fields(self, request, obj=None):
         custom_readonly_fields = ["created_by", "last_modified_by"]
-        readonly_fields = []
         concrete_class_readonly_fields = super(BaseAdmin, self).get_readonly_fields(request, obj)
-        if concrete_class_readonly_fields is None:
-            return custom_readonly_fields
-
-        for field in concrete_class_readonly_fields:
-            readonly_fields.append(field)
-
-        for field in custom_readonly_fields:
-            readonly_fields.append(field)
-
-        return readonly_fields
+        return merge_iterables(concrete_class_readonly_fields, custom_readonly_fields)
 
     # def get_changeform_initial_data(self, request):
     #     data = super(AdminWithUserData, self).get_changeform_initial_data(request)
